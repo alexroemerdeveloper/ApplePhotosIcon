edcfbe1a6d082e706a3fe547171fa934bc64564d
Alexander Römer <alex@roemer.design>
2019-12-24 10:05:23 +0100

    Initial commit

--- ApplePhotosIcon WatchKit Extension/ContentView.swift
+++ ApplePhotosIcon WatchKit Extension/ContentView.swift
@@ -10,7 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack {
+            Circle().frame(width: 75, height: 75).opacity(0)
+            roundRect().foregroundColor(.blue)
+            roundRect().foregroundColor(.green).rotationEffect(.degrees(-90))
+            roundRect().foregroundColor(.pink).rotationEffect(.degrees(90))
+            roundRect().foregroundColor(.orange).rotationEffect(.degrees(180))
+            roundRect().foregroundColor(.green).rotationEffect(.degrees(-45))
+            roundRect().foregroundColor(.purple).rotationEffect(.degrees(45))
+            
+            roundRect().foregroundColor(.yellow).rotationEffect(.degrees(-135))
+            roundRect().foregroundColor(.red).rotationEffect(.degrees(135))
+        }
+    }
+}
+
+struct roundRect: View {
+    var body: some View {
+        Capsule().offset(y: 38).frame(width: 50, height: 70).opacity(0.6)
     }
 }
 
@@ -19,3 +37,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
